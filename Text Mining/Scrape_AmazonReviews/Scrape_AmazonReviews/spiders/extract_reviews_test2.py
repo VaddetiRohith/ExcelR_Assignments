a2143a79e51656606bdedf08548f8a5156c89a9d
Rohith Vaddeti <94513583+VaddetiRohith@users.noreply.github.com>
2023-02-05 16:49:15 +0530

    Add files via upload

---
+++ Text Mining/Scrape_AmazonReviews/Scrape_AmazonReviews/spiders/extract_reviews_test2.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import scrapy
+
+class AmazonReviewsSpider(scrapy.Spider):
+
+    # Spider name
+    name = 'amazon_reviews'
+
+    # Domain names to scrape
+    allowed_domains = ['amazon.in']
+
+    # Base URL for the product reviews
+    myBaseUrl = 'https://www.amazon.in/Bosch-Inverter-Control-Automatic-Loading/product-reviews/B08SR372S7/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber='
+    start_urls=[]
+
+    # Creating list of urls to be scraped by appending page number a the end of base url
+    for i in range(1,121):
+        start_urls.append(myBaseUrl+str(i))
+
+    # Defining a Scrapy parser
+    def parse(self, response):
+            data = response.css('#cm_cr-review_list')
+            
+            # Collecting product star ratings
+            star_rating = data.css('.review-rating')
+
+            # Collecting user reviews
+            comments = data.css('.review-text')
+            count = 0
+
+            # Combining the results
+            for review in star_rating:
+                yield{'stars': 
+                      ''.join(review.xpath('.//text()').extract()),
+                      'comment': 
+                          ''.join(comments[count].xpath(".//text()").extract())
+                     }
+                count=count+1
+
+
+# In[ ]:
+
+
+
+
